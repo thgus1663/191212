770bd6fbd8f699e400c6eeee9cf8f7e52e6a34f1
thgus1663 <remonie7@naver.com>
2019-12-12 17:26:41 +0900

    최종

--- hello.py
+++ hello.py
@@ -110,5 +110,9 @@ def opgg():
     wins = soup.select_one("#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierInfo > span.WinLose > span.wins")
     return render_template('opgg.html', username=username, opgg_url=opgg_url, tier=tier.text, wins=wins.text)
 
+
+
+
+
 if __name__ == '__main__' :
     app.run(debug=True)

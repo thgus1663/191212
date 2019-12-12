6610cf7b22d9b6a5a88520ab93702fb039c71681
thgus1663 <remonie7@naver.com>
2019-12-12 10:17:18 +0900

    route와 함수 관계 배우기

--- hello.py
+++ hello.py
@@ -2,11 +2,18 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #앞에있는 서버주소 생략됨
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
+@app.route('/hi')
+def hi():
+    return 'hi'
+
+@app.route('/thgus')
+def thgus():
+    return 'hello thgus'
 
 if __name__ == '__main__' :
     app.run(debug=True)
